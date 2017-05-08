8b1ad141389ddb80a8b4af402afcc657733ac5b1
Aymanns Florian <faymanns@student.ethz.ch>
2017-05-08 17:41:14 +0200

    added cross over and find best

--- COSgen/algorithms.py
+++ COSgen/algorithms.py
@@ -1,3 +1,4 @@
+import random
 
 class MissingFunction(Exception):
 	pass
@@ -10,9 +11,14 @@ def ga(population,functions,generations=100):
 		raise MissingFunction("No 'cross_over' function in 'functions'.")
 	if len(functions.fitness_measures) == 0:
 		raise MissingFunction("No fitness measures in 'functions'.")
-
+	population_size = len(population)
+	n = int(population_size/2)	#number of sequences surviving each generation
 	for seq in population:
 		seq.fitness = functions.evaluate_fitness(seq)
 	for i in range(generations):
 		best_seqs = functions.find_best(population,n)
-		#cross over, mutate and update the fitnesses
+		population = best_seqs
+		for i in range(population_size-n):
+			population.append(functions.cross_over(best_seqs[random.randrange(n)],best_seqs[random.randrange(n)],mutation_probability=0.5))
+			population[len(population)-1].fitness = functions.evaluate_fitness(population[len(population)-1])
+	return population
