1ae46ab2aa907d3eaab23c2918782935e0f58de8
Aymanns Florian <faymanns@student.ethz.ch>
2017-05-11 10:40:58 +0200

    add type and length arguments in __init__

--- COSgen/sequence.py
+++ COSgen/sequence.py
@@ -1,9 +1,15 @@
 import datetime
+import random
 
 class sequence:
-	def __init__(self):
-		self.l = [1,1,1]
-		self.fitness = 0
+	def __init__(self,seqlen,nstimtypes,seqtype='random'):
+		if seqtype=='random':
+			self.l = random.sample(range(nstimtypes),seqlen)
+		elif seqtype=='block':
+			pass
+		elif seqtype=='m':
+			pass
+		self.fitness = None
 	def dump(self,path):
 		path = path + '/{:%Y%m%d%H%M%S}.tsv'.format(datetime.datetime.now())
 		with open(path,'w+') as f:
