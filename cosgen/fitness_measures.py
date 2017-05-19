b106b185fcd7e3c77c67c63bab5fe748a61b59f4
Aymanns Florian <faymanns@student.ethz.ch>
2017-05-19 15:46:47 +0200

    estimator_variance measure added

--- cosgen/fitness_measures.py
+++ cosgen/fitness_measures.py
@@ -1,2 +1,14 @@
-def test(seq):
-	return sum(seq.l)
+import numpy as np
+
+class OptimalityError(Exception):
+	pass
+
+def estimator_variance(covariance_beta, optimality, contrast=None):
+	if contrast is not None:
+		covaraiance_beta = np.dot(contrast,np.dot(covariance_beta,contrast.transpose()))
+	if optimality=='a':
+		np.trace(covariance_beta)
+	elif optimality=='d':
+		np.linalg.det(covariance_beta)
+	else:
+		raise OptimalityError('Unknow optimality {0}. Possible choices are "a","d","c"'.format(optimality))
