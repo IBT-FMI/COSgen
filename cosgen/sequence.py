ab080ef75b156d5e2957b3a66316e5e829bc0d50
Aymanns Florian <faymanns@student.ethz.ch>
2017-05-29 14:43:44 +0200

    added list argument to __init__

--- cosgen/sequence.py
+++ cosgen/sequence.py
@@ -3,15 +3,20 @@ import random
 import os
 
 class sequence:
-	def __init__(self,seqlen,nstimtypes,seqtype='random'):
-		if seqtype=='random':
+	def __init__(self, seqlen=None, nstimtypes=1, seqtype='random', l=None):
+		if l is not None:
+			self.l = l
+			self.seqlen = len(l)
+		elif seqtype=='random':
 			self.l = [ random.randrange(0,nstimtypes+1,1) for _ in range(seqlen) ]
+			self.seqlen = seqlen
 		elif seqtype=='block':
 			pass
 		elif seqtype=='m':
 			pass
 		self.fitness = None
-	def dump(self,path):
+		self.nstimtypes = nstimtypes
+	def dump(self, path):
 		os.makedirs(path, exist_ok=True)
 		path = path + '/{:%Y%m%d%H%M%S}.tsv'.format(datetime.datetime.now())
 		with open(path,'w+') as f:
