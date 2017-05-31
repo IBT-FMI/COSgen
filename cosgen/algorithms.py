1ac291079d25875369554b29d2a96b85d0f96fe2
Aymanns Florian <faymanns@student.ethz.ch>
2017-05-31 10:29:45 +0200

    basic statistics of ga

--- cosgen/algorithms.py
+++ cosgen/algorithms.py
@@ -1,4 +1,8 @@
 import random
+try:
+	from cosgen.statistics import statistics
+except ImportError:
+	from statistics import statistics
 
 class MissingFunction(Exception):
 	pass
@@ -11,15 +15,18 @@ def ga(population,functions,generations=100):
 		raise MissingFunction("No 'cross_over' function in 'functions'.")
 	if len(functions.fitness_measures) == 0:
 		raise MissingFunction("No fitness measures in 'functions'.")
+	stat = statistics()
 	population_size = len(population)
 	n =  3 #int(population_size/2)	#number of sequences surviving each generation
 	print('n = ',n)
 	for seq in population:
 		seq.fitness = functions.evaluate_fitness(seq)
 	for i in range(generations):
+		stat.add(population)
 		best_seqs = functions.find_best(population,n)
 		population = best_seqs
 		for i in range(population_size-n):
 			population.append(functions.cross_over(best_seqs[random.randrange(n)],best_seqs[random.randrange(n)]))
 			population[len(population)-1].fitness = functions.evaluate_fitness(population[len(population)-1])
+	stat.show()
 	return population
---
+++ cosgen/statistics.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+
+class statistics:
+	def __init__(self):
+		self.max_fitness = []
+		self.average_fitness = []
+		self.population_correlation = []
+
+	def add(self,population):
+		maxfitness = 0
+		avefitness = 0
+		for s in population:
+			if s.fitness > maxfitness:
+				maxfitness = s.fitness
+			avefitness += s.fitness
+		avefitness = avefitness/len(population)
+		self.max_fitness.append(maxfitness)
+		self.average_fitness.append(avefitness)
+		# TODO add population correlation
+
+	def show(self):
+		plt.plot(self.max_fitness)
+		plt.title('Max fitness')
+		plt.show()
+		plt.plot(self.average_fitness)
+		plt.title('Average fitness')
+		plt.show()
