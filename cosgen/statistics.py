4f5a34af8c5c7cdbadc9e5354efa9645d51d8c34
Aymanns Florian <faymanns@student.ethz.ch>
2017-06-07 09:10:46 +0200

    population variance

--- cosgen/statistics.py
+++ cosgen/statistics.py
@@ -1,10 +1,11 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 class Statistics:
 	def __init__(self):
 		self.max_fitness = []
 		self.average_fitness = []
-		self.population_correlation = []
+		self.population_variance = []
 
 	def add(self,population):
 		maxfitness = 0
@@ -16,12 +17,14 @@ class Statistics:
 		avefitness = avefitness/len(population)
 		self.max_fitness.append(maxfitness)
 		self.average_fitness.append(avefitness)
-		# TODO add population correlation
+		self.population_variance.append(1 - np.mean(np.absolute(np.corrcoef([s.l for s in population],rowvar=False))))
 
 	def show(self):
-		plt.plot(self.max_fitness)
-		plt.title('Max fitness')
-		plt.show()
-		plt.plot(self.average_fitness)
-		plt.title('Average fitness')
+		f, axarr = plt.subplots(3,sharex=True)
+		axarr[0].plot(self.max_fitness)
+		axarr[0].set_title('Max fitness')
+		axarr[1].plot(self.average_fitness)
+		axarr[1].set_title('Average fitness')
+		axarr[2].plot(self.population_variance)
+		axarr[2].set_title('Population variance')
 		plt.show()
