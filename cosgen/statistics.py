557a4db446a5045877ba68b57590ad5bce82917f
Aymanns Florian <faymanns@student.ethz.ch>
2017-06-07 14:54:44 +0200

    hamming distance

--- cosgen/statistics.py
+++ cosgen/statistics.py
@@ -1,23 +1,31 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import warnings
+from scipy.spatial.distance import hamming
 
 class Statistics:
 	def __init__(self):
 		self.max_fitness = []
 		self.average_fitness = []
-		self.population_variance = []
+		self.population_diversity = []
 
 	def add(self,population):
+		n = len(population)
 		maxfitness = 0
 		avefitness = 0
+		pop_diversity = 0
 		for s in population:
 			if s.fitness > maxfitness:
 				maxfitness = s.fitness
 			avefitness += s.fitness
-		avefitness = avefitness/len(population)
+		avefitness = avefitness/n
+		for i in range(n):
+			for j in range(i+1,n):
+				pop_diversity += hamming(population[i].l,population[j].l)
+		pop_diversity = pop_diversity/(n*(n-1))*2
 		self.max_fitness.append(maxfitness)
 		self.average_fitness.append(avefitness)
-		self.population_variance.append(1 - np.mean(np.absolute(np.corrcoef([s.l for s in population],rowvar=False))))
+		self.population_diversity.append(pop_diversity)
 
 	def show(self):
 		f, axarr = plt.subplots(3,sharex=True)
@@ -25,6 +33,6 @@ class Statistics:
 		axarr[0].set_title('Max fitness')
 		axarr[1].plot(self.average_fitness)
 		axarr[1].set_title('Average fitness')
-		axarr[2].plot(self.population_variance)
-		axarr[2].set_title('Population variance')
+		axarr[2].plot(self.population_diversity)
+		axarr[2].set_title('Population diversity')
 		plt.show()
