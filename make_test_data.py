d76cb7d8ac2ac175bef7f7d655cbcc6ce300bef6
Aymanns Florian <faymanns@student.ethz.ch>
2017-06-22 10:43:00 +0200

    nifti mask for voxels

--- make_test_data.py
+++ make_test_data.py
@@ -10,14 +10,14 @@ def main(source,destination,sequence,hrf,voxels,scale=1):
 	hrf = np.load(hrf)
 	conv = np.convolve(seq,hrf)
 	func_data = conv/conv.max()/scale
-	vox = np.load(voxels)
+	roi = nib.load(voxels)
+	vox = np.nonzero(roi.get_data())
 	img = nib.load(os.path.expanduser(source))
 	data = img.get_data()
 	conv = np.convolve(seq,hrf)
 	func_data = conv/conv.max()/scale
 	func_data = func_data[0:img.shape[3]]
-	for i in len(vox):
-		data[vox[i,0],vox[i,1],vox[i,2]]+=func_data
+	data[vox] += func_data
 	nib.save(img,os.path.expanduser(destination))
 
 if __name__ == '__main__':
