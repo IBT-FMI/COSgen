4f884344d5f862220d30667ae488aad5313bc147
Aymanns Florian <faymanns@student.ethz.ch>
2017-06-26 15:18:44 +0200

    documentation

--- cosgen/fitness_measures.py
+++ cosgen/fitness_measures.py
@@ -1,10 +1,33 @@
+"""This file contains fitness measure functions."""
 import numpy as np
 
 class OptimalityError(Exception):
+	"""Error raised when a optimality is not 'a' or 'd'."""
 	pass
 
 def estimator_variance(sequence, model, optimality, contrast=None):
+	"""
+	The optimality of the estimator variances.
+	
+	This function calculates the a- or d-optimality value of the
+	covariance matrix of the estimators.
 
+	Parameters
+	----------
+	sequence : cosgen.sequence.Sequence
+	    Sequence for which the covariance matrix is calculated.
+	model : cosgen.models.Model
+	    Model class providing functions for the construction of the
+	    design matrix and covariance matrix.
+	optimality : string
+	    Can be 'a' for a-optimality (trace) or 'd' for d-optimality
+	    (determinat).
+
+	Retruns
+	-------
+	float
+	    optimality value
+	"""
 	X = model.design_matrix(sequence)
 	
 	covariance_beta = model.cov_beta(X)
