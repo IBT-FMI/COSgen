7d845c5d8f4ec0b99c4d3749f44070578015e667
faymanns <faymanns@student.ethz.ch>
2017-07-29 09:49:09 +0200

    fixed contrast matrix

--- examples/estimation.py
+++ examples/estimation.py
@@ -14,7 +14,7 @@ import numpy as np
 from functools import partial
 from scipy.ndimage.filters import gaussian_filter1d
 
-storage_path = '~/.cosgen/estimation'
+storage_path = '~/.cosgen/estimation_FIR10'
 seqlength = 1490
 TR = 1
 nstimtypes = 1
@@ -29,14 +29,17 @@ extra_evs[:,0]=np.ones(seqlength)
 extra_evs[:,1]=np.linspace(-0.5,0.5,seqlength)
 
 def main():
-	basis_set = models.get_FIR_basis_set(32)
-	model = models.EstimationModel(basis_set, whitening_mat=whitening_mat, filterfunc=filter_func, extra_evs=extra_evs)
+	n_basis_vectors= 10
+	basis_set = models.get_FIR_basis_set(n_basis_vectors)
+	model = models.EstimationModel(basis_set, whitening_mat=whitening_mat, filterfunc=filter_func, nonlincorrection=models.squashing_function, extra_evs=extra_evs)
 
 	fc = FunctionCrate()
-	c = np.zeros(34)
-	for i in range(2,34):
-		c[i]=1
-	fc.add_fitness_measure('est_var', partial(fitness_measures.estimator_variance, model=model, optimality='a', contrast=np.matrix(c)))
+	c = np.zeros((n_basis_vectors,n_basis_vectors+2)) #+2 for extra explanatory variables
+	for i in range(n_basis_vectors):
+		c[i,i+2]=1
+	c=np.matrix(c)
+	print(c)
+	fc.add_fitness_measure('est_var', partial(fitness_measures.estimator_variance, model=model, optimality='a', contrast=c))
 	blocksize = estimate_optimal_block_size(seqlength, fc)
 	print('blocksize', blocksize)
 	fc.set_mutate(mutate)
